67f37f7829d07719cb887268f8b54bc73a997677
Saroui <51461462+Saroui@users.noreply.github.com>
2020-11-14 15:08:29 -0500

    Update PC.v

--- PC.v
+++ PC.v
@@ -1,11 +1,14 @@
-module PC(PC_in, clk,PC_out);
+module PC(PC_in, clk, rst,PC_out);
     input [31:0] PC_in;
-    input clk;
+    input clk, rst;
     output reg [31:0] PC_out;
     //reg PC_out;
     
     always @(posedge clk)
     begin 
+    if(rst == 1'b1)
+        PC_out <= 32'b00000000000000000000000000000000;
+    else
         PC_out <= PC_in;
     end
 endmodule
